0827624a062e03769675583f5d362f0f0baa78a2
florinrelea <florinrelea65@gmail.com>
2023-06-17 00:22:46 +0300

    Review structs

--- practice/practice.go
+++ practice/practice.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"example.com/backend"
+	"fmt"
 )
 
+type Dimension struct {
+	length int
+	width int
+	height int
+}
+
+func (d Dimension) Area() int {
+	return d.length * d.width
+}
+
+func (d Dimension) Volume() int {
+	return d.length * d.width * d.height
+}
+
 func main() {
-	backend.Run(":9003")
+	dim := Dimension{length: 10, width: 5, height: 2}
+
+	fmt.Println(dim.Area())
+	fmt.Println(dim.Volume())
 }
