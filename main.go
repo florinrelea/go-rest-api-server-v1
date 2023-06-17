e62d86953db48723af4875e2a4c7147f4a910ed9
florinrelea <florinrelea65@gmail.com>
2023-06-17 20:56:11 +0300

    Refactor main

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
 	"example.com/server"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	rtr := mux.NewRouter()
-	DB, err := sql.Open("sqlite3", "./practice.db")
+	server := server.ServerApp{}
 
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	server := server.ServerApp{
-		Port:   ":9003",
-		Router: rtr,
-		DB:     DB,
-	}
-
-	server.InitRoutes()
+	server.Init(":9003")
 	server.Run()
 }
